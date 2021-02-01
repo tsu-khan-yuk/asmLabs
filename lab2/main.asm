2f61b14f75ef75bfdc39cdee9cbac811dd035820
tsu_khan_yuk <vania8282125@gmail.com>
2021-02-01 03:26:14 +0200

    lab2 wip

--- lab2/main.asm
+++ lab2/main.asm
@@ -1,12 +1,13 @@
 ;; main.asm
-
+; https://prog-cpp.ru/asm-segment/
 STACK_SEG 	SEGMENT PARA STACK "STACK"
-					db  32 dup ("?")
+					db  8 dup ("STACK")
 STACK_SEG 	ENDS
 
 DATA_SEG 	SEGMENT PARA PUBLIC "DATA"
 	hello_string 	db 	14, "Input number: "
 	input_digit		db 	5, ?, 5 dup("?")
+	error_msg 		db 	12, "Invalid data"
 DATA_SEG 	ENDS
 
 
@@ -32,16 +33,86 @@ CODE_SEG 	SEGMENT PARA PUBLIC "CODE"
 		mov 	ah, 10
 		int 	21h
 
+		mov	 	dl,0ah
+	    mov 	ah,02
+	    int 	21h 
+
 		ret
 	INPUT_VALUE 	ENDP
 
 
 	STR_TO_INT 		PROC NEAR
+
+		cmp 	byte ptr [di], '-'
+		jne		short positiv_num
+			mov 	si, 1
+			inc 	di
+		positiv_num:
+		xor 	ax, ax
+		mov 	bx, 10
+		main_array_cycle:
+
+			mov 	cl, [di]
+			cmp 	cl, 0Dh
+			je 		short end_line
+
+			cmp 	cl, '0'
+			jl 		short error
+			cmp 	cl, '9'
+			jg 		short error
+
+			sub 	cl, '0'
+			mul 	bx 		; ax*bx -> (dx, ax)
+			add 	ax, cx
+			inc 	di
+		jmp 	short main_array_cycle
+
+		error:
+			lea 	di, error_msg[1]
+			mov 	cx, 0
+			mov 	cl, error_msg[0]
+			call 	OUTPUT_PROC
+			mov ax, 4c00h
+			int 21h
+
+		end_line:
+		cmp 	si, 1
+		jne 	short not_negative
+			neg 	ax
+		not_negative:
+
 		ret
 	STR_TO_INT		ENDP
 
 
 	INT_TO_STR 		PROC NEAR
+
+		mov 	bx, ax
+		; https://sites.google.com/site/sistprogr/lekcii1/lek8
+		or 	bx, bx
+		jns		positive_value
+			mov 	al, '-'
+			int 	29h
+			neg 	bx
+		positive_value:
+		mov 	ax, bx
+		xor 	cx, cx
+		mov 	bx, 10
+
+		main_calc_cycle:
+			xor 	dx, dx
+			div 	bx 		; http://av-assembler.ru/instructions/div.php
+			add 	dl, '0'
+			push 	dx
+			inc 	cx
+		test 	ax, ax		; http://av-assembler.ru/instructions/test.php
+		jnz 	main_calc_cycle
+
+		print:
+			pop 	ax
+			int 	29h
+		loop 	print
+
 		ret
 	INT_TO_STR 		ENDP
 
@@ -65,27 +136,11 @@ CODE_SEG 	SEGMENT PARA PUBLIC "CODE"
 		lea 	dx, input_digit
 		call 	INPUT_VALUE
 
-		; call 	STR_TO_INT
+		lea 	di, input_digit[2]
+		call 	STR_TO_INT
 
-		mov 	cx ,0
-		lea 	bx, 2
-		num_checking:
+		call 	INT_TO_STR
 
-			; для позитивных значений
-			cmp 	input_digit[bx + cx], '0'
-			jl 		short exit
-
-			cmp 	input_digit[bx + cx], '9'
-			jb 		short exit
-
-			
-
-			inc 	cx
-		cmp 	cl, input_digit[1]
-		jl		short num_checking
-
-		exit:
-		
 		ret
 	MAIN 	ENDP
 
